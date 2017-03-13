2c91a3ac0997a6e243dffb4b22285866d5c4e407
hyd <hydznsqk@163.com>
2017-03-13 16:25:15 +0800

    1. 集成数据库框架'com.j256.ormlite:ormlite-android:5.0' 2. 实现数据库帮助类和创建人员表和收货地址表

--- app/src/main/java/com/yadong/takeout/model/dao/DbHelper.java
+++ app/src/main/java/com/yadong/takeout/model/dao/DbHelper.java
@@ -1,8 +1,55 @@
 package com.yadong.takeout.model.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import com.yadong.takeout.model.dao.bean.AddressBean;
+import com.yadong.takeout.model.dao.bean.UserBean;
+import com.yadong.takeout.ui.app.App;
+import com.yadong.takeout.utils.Constants;
+
+import java.sql.SQLException;
+
 /**
- * Created by Administrator on 2017/1/8.
+ * 数据库的帮助类  用来创建数据库和表
  */
 
-public class DbHelper {
+public class DbHelper extends OrmLiteSqliteOpenHelper {
+
+    private static DbHelper dbHelper;
+
+    private DbHelper(Context context) {
+        super(context, Constants.DB_NAME, null, Constants.DB_VERSION);
+    }
+
+    public static DbHelper getInstance() {
+        if (dbHelper == null) {
+            synchronized (DbHelper.class) {
+                if (dbHelper == null) {
+                    dbHelper = new DbHelper(App.getInstance());
+                    dbHelper.getWritableDatabase();
+                }
+            }
+        }
+        return dbHelper;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+        //创建表
+        try {
+            TableUtils.createTable(connectionSource, UserBean.class);
+            TableUtils.createTable(connectionSource, AddressBean.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/com/yadong/takeout/utils/Constants.java
+++ app/src/main/java/com/yadong/takeout/utils/Constants.java
@@ -7,4 +7,7 @@ package com.yadong.takeout.utils;
 public interface Constants {
 
     String PREFERENCE_NAME = "MyConfig";//Sp存储的文件的名字
+
+    String DB_NAME = "takeOut.db";//数据库的名字
+    int DB_VERSION = 1;//数据库的版本号
 }
