8f4b80c276e638cb5ec680b55c57ecf22913cd5e
houyadong <hydznsqk@163.com>
2017-07-28 18:37:00 +0800

    1. 实现了两个列表的联动处理,并优化其性能

--- app/src/main/java/com/yadong/takeout/ui/adapter/MyHeadAdapter.java
+++ app/src/main/java/com/yadong/takeout/ui/adapter/MyHeadAdapter.java
@@ -1,33 +1,99 @@
 package com.yadong.takeout.ui.adapter;
 
+import android.graphics.Color;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.yadong.takeout.R;
+import com.yadong.takeout.common.app.App;
+import com.yadong.takeout.data.net.bean.StoreMealInfo;
+
+import java.util.List;
+
 
 /**
  *
  */
 
-public class MyHeadAdapter extends BaseAdapter{
+public class MyHeadAdapter extends BaseAdapter {
+
+    private List<StoreMealInfo.GoodsTypeInfo> mHeadData;
+
+    public MyHeadAdapter(List<StoreMealInfo.GoodsTypeInfo> data) {
+        this.mHeadData = data;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return mHeadData == null ? 0 : mHeadData.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mHeadData.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        StoreMealInfo.GoodsTypeInfo data = mHeadData.get(position);
+        HeadViewHolder holder;
+        if (convertView == null) {
+            convertView = new TextView(parent.getContext());
+            holder = new HeadViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (HeadViewHolder) convertView.getTag();
+        }
+
+        holder.setData(data);
+
+        if (position == mSelectedPosition) {
+            holder.itemView.setBackgroundColor(Color.WHITE);
+        }
+
+        return convertView;
+    }
+
+
+    private int mSelectedPosition;
+
+    public void setSelectedPosition(int index) {
+        mSelectedPosition = index;
+        notifyDataSetChanged();
+    }
+
+    class HeadViewHolder {
+
+        private View itemView;
+
+        public HeadViewHolder(View itemView) {
+            this.itemView = itemView;
+        }
+
+        public void setData(StoreMealInfo.GoodsTypeInfo data) {
+            TextView tv = (TextView) this.itemView;
+            tv.setText(data.name);
+            tv.setTextSize(12);
+
+            tv.setBackgroundColor(App.getInstance().getResources().getColor(R.color.colorItemBg));
+
+
+            int h = (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50,
+                    tv.getResources().getDisplayMetrics()) + 0.5f);
+
+            tv.setLayoutParams(new ListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, h));
+            tv.setGravity(Gravity.CENTER);
+        }
     }
 
 }
--- app/src/main/java/com/yadong/takeout/ui/fragment/GoodsFragment.java
+++ app/src/main/java/com/yadong/takeout/ui/fragment/GoodsFragment.java
@@ -1,5 +1,8 @@
 package com.yadong.takeout.ui.fragment;
 
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,6 +16,7 @@ import com.yadong.takeout.data.net.bean.StoreMealInfo;
 import com.yadong.takeout.presenter.contract.GoodsContract;
 import com.yadong.takeout.presenter.fragment.GoodsPresenter;
 import com.yadong.takeout.ui.adapter.MyGroupAdapter;
+import com.yadong.takeout.ui.adapter.MyHeadAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +31,7 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
  *
  */
 
-public class GoodsFragment extends BaseFragment implements GoodsContract.View {
+public class GoodsFragment extends BaseFragment implements GoodsContract.View, AdapterView.OnItemClickListener, AbsListView.OnScrollListener {
 
     @BindView(R.id.lv_left)
     ListView mLeftLv;
@@ -46,6 +50,13 @@ public class GoodsFragment extends BaseFragment implements GoodsContract.View {
 
     private List<StoreMealInfo.GoodsTypeInfo> mHeadData = new ArrayList<>();//头数据集合
     private List<StoreMealInfo.GoodsTypeInfo.GoodsInfo> mBodyData = new ArrayList<>();//普通数据集合
+    private MyHeadAdapter mHeadAdapter;
+    private MyGroupAdapter mGroupAdapter;
+
+    /**
+     * 看是否是用户主动在滑动列表
+     */
+    private boolean isScroll=false;
 
     @Override
     public int setLayout() {
@@ -76,8 +87,8 @@ public class GoodsFragment extends BaseFragment implements GoodsContract.View {
     public void showGroupData(List<StoreMealInfo.GoodsTypeInfo> data, String msg) {
         if (data != null) {
             handleData(data);
-            setLeftLv();
-            setGroupLv();
+            showLeftList();
+            showGroupList();
         } else {
             MyToast.show(getContext(), msg);
         }
@@ -85,6 +96,7 @@ public class GoodsFragment extends BaseFragment implements GoodsContract.View {
 
     /**
      * 处理数据并增加自己这边需要用到的字段到 集合中
+     *
      * @param data
      */
     private void handleData(List<StoreMealInfo.GoodsTypeInfo> data) {
@@ -109,14 +121,68 @@ public class GoodsFragment extends BaseFragment implements GoodsContract.View {
         }
     }
 
-
-    private void setLeftLv() {
-
+    /**
+     * 展示左侧的list
+     */
+    private void showLeftList() {
+        mHeadAdapter = new MyHeadAdapter(mHeadData);
+        mLeftLv.setAdapter(mHeadAdapter);
+        //左侧list条目的点击事件
+        mLeftLv.setOnItemClickListener(this);
     }
 
-    private void setGroupLv() {
-        MyGroupAdapter myGroupAdapter = new MyGroupAdapter(mHeadData, mBodyData);
-        mGroupLv.setAdapter(myGroupAdapter);
+    /**
+     * 展示右侧的list
+     */
+    private void showGroupList() {
+        mGroupAdapter = new MyGroupAdapter(mHeadData, mBodyData);
+        mGroupLv.setAdapter(mGroupAdapter);
+        //右侧list的滑动监听
+        mGroupLv.setOnScrollListener(this);
+    }
+
+    /**
+     * 左侧头列表的点击事件
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //让左侧点击条目背景高亮
+        mHeadAdapter.setSelectedPosition(position);
+        //因为我们的头集合中 存有右侧每个组最顶端的 索引
+        StoreMealInfo.GoodsTypeInfo info = mHeadData.get(position);
+        int groupFirstIndex = info.groupFirstIndex;
+        //右侧列表 跟着滚动定位
+        mGroupLv.setSelection(groupFirstIndex);
+
+        isScroll=false;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        /**
+         * 看是否是用户主动在滑动列表,要不然会出现刷新adapter多次
+         * 因为一点击左边头条目,右侧就会跟着动,跟着动就会触发监听,
+         * 然后又刷新左侧adapter,而且这个scroll监听很频繁,会一直刷新左边列表
+         */
+        isScroll=true;
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        //如果是用户主动在滑列表,才会去定位左侧的头条目
+        if(isScroll){
+            StoreMealInfo.GoodsTypeInfo.GoodsInfo data = mBodyData.get(firstVisibleItem);
+            // 当前正在置顶显示的头高亮处理
+            mHeadAdapter.setSelectedPosition(data.headIndex);
+
+            // 判断头容器对应的条目是否处于可见状态
+            // 获取到第一个可见，和最后一个可见的。比第一个小的，或者比最后一个大的均为不可见
+            int firstVisiblePosition = mLeftLv.getFirstVisiblePosition();
+            int lastVisiblePosition = mLeftLv.getLastVisiblePosition();
+            if (data.headIndex <= firstVisiblePosition || data.headIndex >= lastVisiblePosition) {
+                mLeftLv.setSelection(data.headIndex);// 可见处理
+            }
+        }
     }
 
 
